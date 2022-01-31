c3691fac6024dd91f9dee40276cb1778075f1213
David Berka <davidberka2005@gmail.com>
2022-01-31 21:43:37 +0100

    HP Mobile - Done

---
+++ js/main.js
@@ -0,0 +1,32 @@
+function menuHandler() {
+    const burgerMenu = document.querySelector(".burger");
+    const slideNav = document.querySelector(".header_nav");
+
+    burgerMenu.addEventListener("click", () => {
+    slideNav.classList.toggle("active");
+    burgerMenu.classList.toggle("active");
+    });
+
+    if (window.innerWidth < 1024) {
+    const navLinks = document.querySelectorAll(".nav_links > li > a");
+    const dropdownMenus = document.querySelectorAll(".dropdown_menu");
+    
+    navLinks.forEach((link) => {
+        link.addEventListener("click", () => {
+        // remove active className
+        dropdownMenus.forEach((menu) => {
+            if (link.parentNode != menu.parentNode) {
+            menu.classList.remove("active");
+            }
+        });
+        // add active className to current dropdown
+        const dropdownMenu = link.nextElementSibling;
+        if (dropdownMenu) {
+            dropdownMenu.classList.toggle("active");
+        }
+        });
+    });
+    }
+}
+
+menuHandler();
